cd5ad5050e5a4fb04eb72f6ea03d35cc53a697c5
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-04-13 14:33:09 +0200

    after 07 april class

--- public/app.js
+++ public/app.js
@@ -1,20 +1,29 @@
-function createPost(data){
+function createAuthor(data){
 	var p = document.createElement('p');
 	p.textContent = data.author;
-
 	return p;
 }
-
+function createContent(data){
+	var p = document.createElement('p');
+	p.textContent = data.content;
+	return p;
+}
+function createTitle(data){
+	var p = document.createElement('p');
+	p.textContent = data.title;
+	return p;
+}
 function success(data){
-
-	for (var i = 0; i < data.lenght; i++){
+	for (var i = 0; i < data.length; i++){
 		var post = data[i];
-		var postElem = createPost(post);
-
-		//dodać do dom
+		var postElementAuthor = createAuthor(post);
+		var postElementContent = createContent(post);
+		var postElementTitle = createTitle(post);
+		var div = document.getElementsByTagName('div')[0];
+		div.appendChild(postElementAuthor);
+		div.appendChild(postElementTitle);
+		div.appendChild(postElementContent);
 	}
-
-	console.log(data);
 }
 
 $.ajax({
