0025f0cb2293c4b6e274c286de2b3c24ac4b5c2f
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-04-19 10:52:24 +0200

    after class 14.04 and adding bootstrap

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
+
 function createAuthor(data){
-	var p = document.createElement('p');
-	p.textContent = data.author;
-	return p;
+	var h3 = document.createElement('h3');
+	h3.textContent = data.author;
+	return h3;
 }
 function createContent(data){
 	var p = document.createElement('p');
@@ -9,24 +10,41 @@ function createContent(data){
 	return p;
 }
 function createTitle(data){
-	var p = document.createElement('p');
-	p.textContent = data.title;
-	return p;
+	var h4 = document.createElement('h4');
+	h4.textContent = data.title;
+	return h4;
 }
 function success(data){
+	var div = document.getElementsByTagName('div')[1];
+	div.innerHTML = '';
 	for (var i = 0; i < data.length; i++){
 		var post = data[i];
 		var postElementAuthor = createAuthor(post);
 		var postElementContent = createContent(post);
 		var postElementTitle = createTitle(post);
-		var div = document.getElementsByTagName('div')[0];
 		div.appendChild(postElementAuthor);
 		div.appendChild(postElementTitle);
 		div.appendChild(postElementContent);
 	}
 }
-
-$.ajax({
-	url: '/posts',
-	success: success
+var form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', function(e) {
+	$.ajax({
+		url: '/posts',
+		method: 'POST',
+		data: $(this).serialize(),
+		success: loadPosts	
+	});
+	form.reset();
+	e.preventDefault();
 });
+
+function loadPosts()
+{
+	$.ajax({
+		url: '/posts',
+		success: success
+	});
+}
+
+loadPosts();
